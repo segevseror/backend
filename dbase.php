591aaad14d03d41facf404e087bde96d13fa8b40
שגב סרור <segev@wisegroup.co.il>
2019-12-04 23:07:39 +0200

    first up

--- dbase.php
+++ dbase.php
@@ -1,29 +1,28 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'golball';
-$user = 'root';
-$pass = '';
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    global $conn;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-//heroku
-//    $host        = "host = ec2-54-217-234-157.eu-west-1.compute.amazonaws.com";
-//    $port        = "port = 5432";
-//    $dbname      = "dbname = dav5nd421ipu74";
-//    $credentials = "user = lfpegwcrzqkfgp password=52bc85c5bec01bb9563d98e20dcad713319986829d6321939892fe4f3787227c";
+// $host = 'localhost';
+// $dbname = 'golball';
+// $user = 'root';
+// $pass = '';
+// try {
+//     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+//     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+//     global $conn;
+// } catch (PDOException $e) {
+//     print "Error!: " . $e->getMessage() . "<br/>";
+//     die();
+// }
+   $host        = "host = ec2-54-217-234-157.eu-west-1.compute.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dav5nd421ipu74";
+   $credentials = "user = lfpegwcrzqkfgp password=52bc85c5bec01bb9563d98e20dcad713319986829d6321939892fe4f3787227c";
 
-//    $db = pg_connect( "$host $port $dbname $credentials"  );
-//    if(!$db) {
-//       echo "Error : Unable to open database\n";
-//    } else {
-//     global $db;
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+    global $db;
   
-//    }
+   }
 
 ?>
