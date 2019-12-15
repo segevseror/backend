49ba13220bbdbff81b335a3228aa6a71291456f2
שגב סרור <segev@wisegroup.co.il>
2019-12-15 20:42:42 +0200

    fix header req

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Content-type: application/json');
 session_start ();
 error_reporting(0);
 require 'dbase.php';
